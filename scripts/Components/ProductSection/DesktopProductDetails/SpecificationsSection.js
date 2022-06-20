4d246f765ddd810f7829095b9f4a6f61b84aad42
Kishan <tech@andus.agency>
2022-06-20 16:15:45 +0530

    Fixed login and register page QA issues

--- scripts/Components/ProductSection/DesktopProductDetails/SpecificationsSection.js
+++ scripts/Components/ProductSection/DesktopProductDetails/SpecificationsSection.js
@@ -89,7 +89,7 @@ const SpecificationsSection = ({ productBlock, frameSelected, frameColorSelected
   return (
     <>
     <div className="pdp-tabs-sec pt-16 text-sm justify-items-center max-w-screen-xxl font-extralight items-center flex flex-row xl:w-10/12 gap-20">
-      <div className="mx-auto lg:col-span-2 lg:grid lg:grid-cols-2 gap-5 max-w-screen-xxl">
+      <div className="mx-auto md:col-span-2 md:grid md:grid-cols-2 gap-5 max-w-screen-xxl">
         <div className="dimensions-col">
         <SectionName>{dimensions_heading}</SectionName>
           <ColumnOne>
