cb7fd710aa827c51ad81a2c899cf21ba0644c989
Kishan <tech@andus.agency>
2022-06-29 21:34:12 +0530

    Removed unused JS for SSO

--- scripts/Components/Header/Header.js
+++ scripts/Components/Header/Header.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Nav from './Nav';
 import MobileNav from './MobileNav';
-
+import CartHighlight from '../CartHighlight';
 
 const Header = (props) => {
-  const { themeSettings, mobileNavSettings, cartCount } = props;
+  const { themeSettings, newsettings, mobileNavSettings, cartCount } = props;
+  const [isShown, setIsShown] = useState(false);
   const [sticky, setSticky] = useState(false);
   const handleScroll = () => {
     setSticky(window.pageYOffset > 50);
